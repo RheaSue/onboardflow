0fb336ee19df7eb06d078307502827eeeaaa1f3a
Rhea <suli@sz.hitrontech.com>
2024-11-26 10:44:01 +0800

    Set bg_750 image as bgImage for Footer

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ function renderContent(msoName, deviceType, language) {
 
 	</main>
 
-	<footer>
+	<footer style="background-image: url("/src/images/general/bg_750.png")>
 		<div>
 			<span class="ultralight-text">${localizedTexts.footerFirstLine}</span>
 			<br />
